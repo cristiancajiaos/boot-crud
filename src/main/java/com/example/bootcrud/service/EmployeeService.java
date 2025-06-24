e47b8c2a72c8768e21226797df8d1cef798fb2eb
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-24 18:25:02 -0400

    Creado servicio de empleados

---
+++ src/main/java/com/example/bootcrud/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.bootcrud.service;
+
+import com.example.bootcrud.dto.EmployeeDto;
+
+public interface EmployeeService {
+  EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+}
---
+++ src/main/java/com/example/bootcrud/service/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.bootcrud.service;
+
+import com.example.bootcrud.dto.EmployeeDto;
+import com.example.bootcrud.entity.Employee;
+import com.example.bootcrud.mapper.EmployeeMapper;
+import com.example.bootcrud.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+
+  @Override
+  public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+    Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+    Employee createdEmployee = employeeRepository.save(employee);
+    return EmployeeMapper.mapToEmployeeDto(createdEmployee);
+  }
+}
