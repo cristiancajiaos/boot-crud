b01233a6b9971c45bc914ba4dde39e49d7b383e0
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-24 18:11:19 -0400

    Corrección menor

--- src/main/java/com/example/bootcrud/mapper/EmployeeMapper.java
+++ src/main/java/com/example/bootcrud/mapper/EmployeeMapper.java
@@ -5,7 +5,7 @@ import com.example.bootcrud.entity.Employee;
 
 public class EmployeeMapper {
   public static EmployeeDto mapToEmployeeDto(Employee employee) {
-    return new EmployeeDto(employee.getId(), employee.getName(), employee.getDepartment())
+    return new EmployeeDto(employee.getId(), employee.getName(), employee.getDepartment());
   }
 
   public static Employee mapToEmployee(EmployeeDto employeeDto) {
